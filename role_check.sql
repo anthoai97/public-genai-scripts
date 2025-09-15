6f62bcf6d3fe21230c17160c4db9fd56548bc6bf
An Quách <anquach.dev@gmail.com>
2025-09-15 17:51:22 +0700

    Update role_check.sql

--- role_check.sql
+++ role_check.sql
@@ -3,3 +3,14 @@ FROM pg_default_acl d
 JOIN pg_roles r ON r.oid = d.defaclrole
 LEFT JOIN pg_namespace n ON n.oid = d.defaclnamespace
 WHERE d.defaclacl::text LIKE '%role_a%';
+
+SELECT r.rolname
+FROM pg_auth_members m
+JOIN pg_roles r ON m.roleid = r.oid
+WHERE m.member = (SELECT oid FROM pg_roles WHERE rolname = CURRENT_USER);
+
+SELECT grantee, privilege_type
+FROM information_schema.role_table_grants
+WHERE table_schema = 'public'
+  AND table_name = 'mytable'
+  AND grantee IN (CURRENT_USER, 'PUBLIC');
