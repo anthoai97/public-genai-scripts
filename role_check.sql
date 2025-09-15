1d03dd7bb448a8112ce6e2033694241a8c8f6652
An Quách <anquach.dev@gmail.com>
2025-09-15 19:00:22 +0700

    Update role_check.sql

--- role_check.sql
+++ role_check.sql
@@ -14,3 +14,19 @@ FROM information_schema.role_table_grants
 WHERE table_schema = 'public'
   AND table_name = 'mytable'
   AND grantee IN (CURRENT_USER, 'PUBLIC');
+
+
+SELECT r.usename AS grantor,
+       e.usename AS grantee,
+       nspname,
+       privilege_type,
+       is_grantable
+  FROM pg_namespace
+JOIN LATERAL (SELECT *
+                FROM aclexplode(nspacl) AS x) a
+          ON true
+        JOIN pg_user e
+          ON a.grantee = e.usesysid
+        JOIN pg_user r
+          ON a.grantor = r.usesysid 
+       WHERE e.usename = 'YOUR_USER';
